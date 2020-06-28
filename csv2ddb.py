575e7d40badf64de838ca44a94801377c63ae727
Gregory Lindsey <gclindsey@gmail.com>
2020-06-27 17:09:03 -0700

    Test create_table, load in cli

--- csv2ddb.py
+++ csv2ddb.py
@@ -1,20 +1,106 @@
-# import click
-
-# @click.command()
-# @click.option('--count', default=1, help='Number of greetings.')
-# @click.option('--name', prompt='Your name', help='The person to greet.')
-
-# def hello(count, name):
-#     """Simple program that greets NAME for a total of COUNT times."""
-#     for x in range(count):
-#         click.echo('Hello %s!' % name)
-
-# if __name__ == '__main__':
-#     hello()
-
 import click
+import boto3
+import csv
 
-@click.command()
+@click.group()
 def cli():
-    """Example script."""
-    click.echo('Hello World!')
+    pass
+
+@cli.command()
+@click.option("--table-name", required=True, type=click.STRING) # type=click.File("rb"), nargs=-1
+@click.option("--primary-key", required=True, type=click.STRING)
+@click.option("--sort-key", required=True, type=click.STRING)
+@click.option("--sort-key-type", required=True, type=click.STRING)
+def table(table_name, primary_key, sort_key, sort_key_type): 
+
+    dynamodb_client = boto3.client('dynamodb')
+    existing_tables = dynamodb_client.list_tables()['TableNames']
+
+    if table_name not in existing_tables:
+        # print("Creating Table: " + table_name + " ...")
+        # print("Primary key: " + primary_key)
+        # print("Sort key: " + sort_key)
+
+        response = dynamodb_client.create_table(
+            TableName=table_name,
+            KeySchema=[
+                {
+                    'AttributeName': primary_key,
+                    'KeyType': 'HASH'
+                },
+                {
+                    'AttributeName': sort_key,
+                    'KeyType': 'RANGE'
+                }
+            ],
+            AttributeDefinitions=[
+                {
+                    'AttributeName': primary_key,
+                    'AttributeType': 'S'
+                },
+                {
+                    'AttributeName': sort_key,
+                    'AttributeType': sort_key_type
+                }
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 5,
+                'WriteCapacityUnits': 5
+            }
+        )
+        print("Created table: " + table_name)
+    else:
+        print("Table " + table_name + " already exists.")
+
+
+@cli.command()
+@click.argument("filename", nargs=1)
+@click.option("--table-name", required=True, type=click.STRING)
+def load(filename, table_name):
+    click.echo(filename)
+
+    def csv_to_dict(filename):
+        items = []
+        with open(filename) as csvfile:
+            print("Opening " + filename + "...")
+            reader = csv.DictReader(csvfile)
+            index = reader.fieldnames
+            print("Columns: ", index)
+            for row in reader:
+                data = {}
+                
+                # Change User to integer type
+                for col in index:
+                    data[col] = row[col]
+                    if col == 'User':
+                        data[col] = int(data[col])
+            
+                items.append(data)
+
+        # Replace empty string values with None type
+        for row in items:
+            for key, value in row.items():
+                if value == '':
+                    row[key] = None
+
+        return items
+
+    def dict_to_dynamodb(items, table_name):
+        
+        session = boto3.Session()
+        print("Created session...")
+
+        dynamodb = session.resource('dynamodb')
+        db = dynamodb.Table(table_name)
+        print("Loading " + table_name + "...")
+        print(db.key_schema)
+
+        with db.batch_writer() as batch:
+            for item in items:
+                #print(item)
+                batch.put_item(Item=item)
+
+        return print(table_name + " has been loaded.")
+
+    items = csv_to_dict(filename)
+    dict_to_dynamodb(items, table_name)
--- setup.py
+++ setup.py
@@ -1,19 +1,3 @@
-# from setuptools import setup
-
-# setup(
-#     name='csv2ddb',
-#     version='0.1',
-#     py_modules=['app'],
-#     install_requires=[
-#         'Click',
-#     ],
-#     entry_points='''
-#         [console_scripts]
-#         app=app:cli
-#     ''',
-
-# )
-
 from setuptools import setup
 
 setup(
@@ -22,6 +6,8 @@ setup(
     py_modules=['csv2ddb'],
     install_requires=[
         'Click',
+        'boto3',
+        'awscli',
     ],
     entry_points='''
         [console_scripts]
